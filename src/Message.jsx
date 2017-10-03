885753ea0fce8e44f9f4f9d713d5b078e5af6944
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-02 19:22:13 -0700

    Defined Message with username and content from messageBody

---
+++ src/Message.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <div className="message">
+        <span className="message-username">{this.props.messageBody.username}</span>
+        <span className="message-content">{this.props.messageBody.content}</span>
+      </div>
+    );
+  }
+}
+export default Message;
