d6c29d4d0e55257d4351959d2baaa1df6da0ba5c
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-03 11:51:40 -0700

    Defined sendMessage and setUsername to pass message or username change through to parent container

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,11 +2,34 @@ import React, {Component} from 'react';
 
 class ChatBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.sendMessage = this.sendMessage.bind(this);
+    this.setUsername = this.setUsername.bind(this);
+  }
+
+  sendMessage(e) {
+    let keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+      this.props.addMessage(this.props.currentUser.name, e.target.value);
+      e.target.value = '';
+    }
+  }
+
+  setUsername(e) {
+    let keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+      this.props.setUsername(e.target.value);
+      e.target.value = e.target.value;
+    }
+  }
+
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" onKeyUp={this.props.handleEnter} placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" onKeyUp={this.setUsername} defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" onKeyUp={this.sendMessage} placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
