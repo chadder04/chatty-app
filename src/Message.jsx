d1df4d4824c7ee4bb448ea0ca4d1be1a8a380943
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-03 17:04:30 -0700

    Testing message for img url, and if so, add ImageMessage component

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import ImageMessage from './ImageMessage.jsx';
 
 class Message extends Component {
   render() {
+    let testForURL = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/.test(this.props.messageBody.content);
+    let messageBody = (testForURL) ? <ImageMessage imgSrc="${this.props.messageBody.content}" /> : this.props.messageBody.content;
+    let userNameStyle = {
+      color: this.props.messageBody.usernameColor
+    }
     return (
       <div className="message">
-        <span className="message-username">{this.props.messageBody.username}</span>
-        <span className="message-content">{this.props.messageBody.content}</span>
+        <span className="message-username" style={userNameStyle}>{this.props.messageBody.username}</span>
+        <span className="message-content">{messageBody}</span>
       </div>
     );
   }
