b6cdbde28418c439a89acea7e4cbdf538c0fd4b5
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-03 19:09:43 -0700

    Fixed ImageMessage not being printed correctly, updated usernameColor to color

--- client/src/Message.jsx
+++ client/src/Message.jsx
@@ -4,9 +4,9 @@ import ImageMessage from './ImageMessage.jsx';
 class Message extends Component {
   render() {
     let testForURL = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/.test(this.props.messageBody.content);
-    let messageBody = (testForURL) ? <ImageMessage imgSrc="${this.props.messageBody.content}" /> : this.props.messageBody.content;
+    let messageBody = (testForURL) ? <ImageMessage imgSrc={ this.props.messageBody.content } /> : this.props.messageBody.content;
     let userNameStyle = {
-      color: this.props.messageBody.usernameColor
+      color: this.props.messageBody.color
     }
     return (
       <div className="message">
