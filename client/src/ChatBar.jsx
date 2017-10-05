9888e73977687b308de4fa4ee8e72cfa54dbe90c
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-04 18:13:59 -0700

    Added refs to username and messagebar, altered keycode to use e.key instead

--- client/src/ChatBar.jsx
+++ client/src/ChatBar.jsx
@@ -10,27 +10,26 @@ class ChatBar extends Component {
   }
 
   sendMessage(e) {
-    let keycode = (e.keyCode ? e.keyCode : e.which);
-    if (keycode == '13') {
+    if (e.key === 'Enter') {
       this.props.addMessage(this.props.currentUser.name, e.target.value, 'postMessage');
       e.target.value = '';
     }
   }
 
   setUsername(e) {
-    let keycode = (e.keyCode ? e.keyCode : e.which);
-    if (keycode == '13') {
+    if (e.key === 'Enter') {
       this.props.setUsername(e.target.value, this.props.currentUser.color);
       this.props.addMessage(e.target.value, `${this.props.currentUser.name} changed their name to ${e.target.value}`, 'postNotification')
       e.target.value = e.target.value;
+      this.refs.messagebar.focus();
     }
   }
 
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" onKeyUp={this.setUsername} defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" onKeyUp={this.sendMessage} placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" onKeyUp={this.setUsername} defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" ref="username"/>
+        <input className="chatbar-message" onKeyUp={this.sendMessage} placeholder="Type a message and hit ENTER" ref="messagebar" />
       </footer>
     );
   }
