5d4392254c140ee55baa84db0d5cea345b2d09d1
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-05 17:26:05 -0700

    Moved userNameStyle into <span> element directly

--- client/src/Message.jsx
+++ client/src/Message.jsx
@@ -5,12 +5,9 @@ class Message extends Component {
   render() {
     let testForURL = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/.test(this.props.messageBody.content);
     let messageBody = (testForURL) ? <ImageMessage imgSrc={ this.props.messageBody.content } /> : this.props.messageBody.content;
-    let userNameStyle = {
-      color: this.props.messageBody.color
-    }
     return (
       <div className="message">
-        <span className="message-username" style={userNameStyle}>{this.props.messageBody.username}</span>
+        <span className="message-username" style={{ color: this.props.messageBody.color }}>{this.props.messageBody.username}</span>
         <span className="message-content">{messageBody}</span>
       </div>
     );
